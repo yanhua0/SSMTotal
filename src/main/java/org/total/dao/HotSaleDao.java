c8941e99c363094bf858543d471a1ec3f377420b
your name <your_email@youremail.com>
2018-05-15 13:24:11 +0800

    邮箱验证，秒滴短信验证接口与页面结合

--- src/main/java/org/total/dao/HotSaleDao.java
+++ src/main/java/org/total/dao/HotSaleDao.java
@@ -1,5 +1,6 @@
 package org.total.dao;
 
+import org.apache.ibatis.annotations.Param;
 import org.total.entity.HotSale;
 
 import java.util.List;
@@ -8,4 +9,5 @@ public interface HotSaleDao {
     List<HotSale> queryAll();
     HotSale queryById(int id);
     List<HotSale> queryById2(int id);
+    HotSale queryByIdOrNumber(String id);
 }
--- src/test/java/org/total/dao/HotSaleDaoTest.java
+++ src/test/java/org/total/dao/HotSaleDaoTest.java
@@ -7,9 +7,9 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.total.entity.HotSale;
 
+import java.util.HashMap;
 import java.util.List;
 
-import static org.junit.Assert.*;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({"classpath:spring/spring-dao.xml"})
 public class HotSaleDaoTest {
@@ -18,6 +18,26 @@ public class HotSaleDaoTest {
     @Test
     public void queryAll() {
         List<HotSale> hotSale=hotSaleDao.queryAll();
+        System.out.println(hotSale);
+    }
+    @Test
+    public void map()
+    {
+        HashMap<String,String> map=new HashMap<String,String>();
+        map.put("1","重庆");
+        map.put("2","北京");
+        map.put(null,null);
+//        Object[] obj= (Object[]) map.get(key);
+//        System.out.println("key= "+key+" and value= "+map.get(key)+"and object="+ Arrays.toString(obj));
+        for(String key :map.keySet())
+        {
+            System.out.println("key="+key+"value="+ map.get(key));
+        }
+    }
+
+    @Test
+    public void queryByIdOrNumber() {
+        HotSale hotSale=hotSaleDao.queryByIdOrNumber("2");
         System.out.println(hotSale);
     }
 }
