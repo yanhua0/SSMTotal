3446281a9f44e9d734d068faea8bfd18b52934b4
your name <your_email@youremail.com>
2018-07-27 18:44:48 +0800

    @ResonsBody乱码解决

--- src/main/java/mybatis/entity/user.java
+++ src/main/java/mybatis/entity/user.java
@@ -13,31 +13,43 @@ public class User {
 
     private Date createTime;
 
-    public User(String username, String password, Integer state, String phone, Date createTime) {
-        this.username = username;
-        this.password = password;
-        this.state = state;
-        this.phone = phone;
-        this.createTime = createTime;
-    }
-
     public String getUsername() {
         return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username == null ? null : username.trim();
     }
 
     public String getPassword() {
         return password;
     }
 
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
     public Integer getState() {
         return state;
+    }
+
+    public void setState(Integer state) {
+        this.state = state;
     }
 
     public String getPhone() {
         return phone;
     }
 
+    public void setPhone(String phone) {
+        this.phone = phone == null ? null : phone.trim();
+    }
+
     public Date getCreateTime() {
         return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
     }
 }
--- src/main/java/org/total/web/TestController.java
+++ src/main/java/org/total/web/TestController.java
@@ -1,26 +1,45 @@
 package org.total.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.total.entity.HotSale;
+import org.total.service.TotalService;
 
 
 @Controller
 public class TestController {
-
+    @Autowired
+    private TotalService totalService;
 
     @RequestMapping(value="/test",method=RequestMethod.GET)
     public String reg22(){
     	return "test";
     }
+    /*
+    单独字符串要加
+     */
     @RequestMapping(value="/test1",method=RequestMethod.GET,produces = "text/html;charset=UTF-8")
     @ResponseBody
     public String reg1(@RequestParam("username")String username){
     	System.out.println(username);
     	return  username;
     }
+    @RequestMapping(value="/test2",method=RequestMethod.POST)
+    @ResponseBody
+    public HotSale reg111121(){
+      HotSale h=totalService.queryById(1);
 
+        return h;
+    }
+    @RequestMapping(value="/test2",method=RequestMethod.GET)
+    @ResponseBody
+    public HotSale reg231(){
+        HotSale h=totalService.queryById(1);
+        return h;
+    }
 }
