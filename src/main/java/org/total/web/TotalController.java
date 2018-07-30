6a9037893bc71065d9674dcb08e0b8364e1ff86a
your name <your_email@youremail.com>
2018-07-30 14:36:08 +0800

    useGeneratedKeys="true" keyProperty="id"  插入数据返回主键id

--- src/main/java/org/total/web/TotalController.java
+++ src/main/java/org/total/web/TotalController.java
@@ -27,41 +27,48 @@ import java.util.List;
 public class TotalController {
     @Autowired
     private TotalService totalService;
-    @RequestMapping(value = "/advice",method = RequestMethod.GET)
-    public String getSomething(@RequestParam("msg") String msg){
+
+    @RequestMapping(value = "/advice", method = RequestMethod.GET)
+    public String getSomething(@RequestParam("msg") String msg) {
 
         throw new RuntimeException();
     }
+
     @RequestMapping
-    public String Index()
-    {
+    public String Index() {
         return "index";
     }
-    @RequestMapping(value = "/login",method = RequestMethod.GET)
-    public String PageFirsr()
-    {
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String PageFirsr() {
         return "login";
     }
-    @RequestMapping(value = "/check",method = RequestMethod.POST)
-    public String dd(HttpSession session)
-    {   String username="123";
-        session.setAttribute("username",username);
-        System.out.println("拦截器不能拦截的请求!!!username="+session.getAttribute("username"));
+
+    @RequestMapping(value = "/check", method = RequestMethod.POST)
+    public String dd(HttpSession session) {
+        String username = "123";
+        session.setAttribute("username", username);
+        System.out.println("拦截器不能拦截的请求!!!username=" + session.getAttribute("username"));
         return "redirect:/total/pagehelper";
     }
-    @RequestMapping(value = "/pagehelper",method = RequestMethod.GET)
-    public String fenye(@RequestParam(value = "page",defaultValue = "1",required = true) Integer page,
-                        @RequestParam(value = "id",defaultValue = "0",required = true) int id,Model model)
-    {   PageHelper.startPage(page, 3);
-        List<HotSale> hotSale=totalService.queryById2(id);
-        PageInfo<HotSale> p=new PageInfo<HotSale>(hotSale);
-        model.addAttribute("page",p);
-        model.addAttribute("hotsale",hotSale);
+
+    @RequestMapping(value = "/pagehelper", method = RequestMethod.GET)
+    public String fenye(@RequestParam(value = "page", defaultValue = "1", required = true) Integer page,
+                        @RequestParam(value = "id", defaultValue = "0", required = true) int id, Model model) {
+        PageHelper.startPage(page, 3);
+        List<HotSale> hotSale = totalService.queryById2(id);
+        PageInfo<HotSale> p = new PageInfo<HotSale>(hotSale);
+        model.addAttribute("page", p);
+        model.addAttribute("hotsale", hotSale);
         return "list";
     }
-    @RequestMapping(value = "/up",method = RequestMethod.GET)
-    public String up(){return "upload";}
-    @RequestMapping(value = "/upload",method = RequestMethod.POST)
+
+    @RequestMapping(value = "/up", method = RequestMethod.GET)
+    public String up() {
+        return "upload";
+    }
+
+    @RequestMapping(value = "/upload", method = RequestMethod.POST)
     @ResponseBody
     public String upload(@RequestParam("file") MultipartFile[] file, HttpServletRequest request) throws IOException {
         String path = request.getSession().getServletContext().getRealPath("../../src/main/webapp/resources/uploadfile");
@@ -76,20 +83,21 @@ public class TotalController {
 
             //MultipartFile自带的解析方法
         }
-        System.out.println("上传成功!"+path);
+        System.out.println("上传成功!" + path);
         return "upload.jsp";
     }
+
     @RequestMapping("/down")
-    public void down(@RequestParam("file") String file,HttpServletRequest request,HttpServletResponse response) throws Exception{
+    public void down(@RequestParam("file") String file, HttpServletRequest request, HttpServletResponse response) throws Exception {
         //模拟文件，myfile.txt为需要下载的文件
 
-        String fileName = request.getSession().getServletContext().getRealPath("../../src/main/webapp/resources/uploadfile")+"/"+file;
+        String fileName = request.getSession().getServletContext().getRealPath("../../src/main/webapp/resources/uploadfile") + "/" + file;
         //获取输入流
         InputStream bis = new BufferedInputStream(new FileInputStream(new File(fileName)));
         //假如以中文名下载的话
         String filename = file;
         //转码，免得文件名中文乱码
-        filename = URLEncoder.encode(filename,"UTF-8");
+        filename = URLEncoder.encode(filename, "UTF-8");
         //设置文件下载头
         response.addHeader("Content-Disposition", "attachment;filename=" + filename);
         //1.设置文件ContentType类型，这样设置，会自动判断下载文件类型
@@ -98,17 +106,18 @@ public class TotalController {
 
         int len = 0;
 
-        while((len = bis.read()) != -1){
+        while ((len = bis.read()) != -1) {
             out.write(len);
             out.flush();
         }
         out.close();
 
     }
+
     @RequestMapping(value = "/exposer")
-    public TestResult<Exposer> exposer(){
-        Exposer exposer=totalService.exposerUrl(1,"1","2");
-        return new TestResult<Exposer>(false,exposer);
+    public TestResult<Exposer> exposer() {
+        Exposer exposer = totalService.exposerUrl(1, "1", "2");
+        return new TestResult<Exposer>(false, exposer);
     }
 
 }
--- src/main/java/org/total/web/UserController.java
+++ src/main/java/org/total/web/UserController.java
@@ -17,82 +17,80 @@ import java.util.Map;
 public class UserController {
     @Autowired
     private UserMapper userMapper;
-    @RequestMapping(value = "/user",method = RequestMethod.POST)
-    @ResponseBody
-    public void Entity(User user)
-    {
 
-        System.out.println("user{}"+user);
+    @RequestMapping(value = "/user", method = RequestMethod.POST)
+    @ResponseBody
+    public void Entity(User user) {
+
+        System.out.println("user{}" + user);
 
     }
-/*
-前端不能为空字符串
- */
-    @RequestMapping(value = "/map",method = RequestMethod.GET)
+
+    /*
+    前端不能为空字符串
+     */
+    @RequestMapping(value = "/map", method = RequestMethod.GET)
     @ResponseBody
-    public void Entity2(@RequestParam  Map<String,String> map)
-    {     if(map.containsKey("username"))
-         {
-        int username=Integer.parseInt(map.get("username"));
+    public void Entity2(@RequestParam Map<String, String> map) {
+        if (map.containsKey("username")) {
+            int username = Integer.parseInt(map.get("username"));
         }
-        if(map.containsKey("password"))
-        {
-            String username=map.get("password");
+        if (map.containsKey("password")) {
+            String username = map.get("password");
         }
         System.out.println(map);
-       for(String key:map.keySet())
-       {
-           System.out.println("key="+key+"value="+map.get(key));
-       }
+        for (String key : map.keySet()) {
+            System.out.println("key=" + key + "value=" + map.get(key));
+        }
     }
+
     /*
     映射.html文件
      */
-    @RequestMapping(value = "/html",method = RequestMethod.GET)
-    public String html()
-    {
+    @RequestMapping(value = "/html", method = RequestMethod.GET)
+    public String html() {
         return "hello";
     }
-/*
-两种方式获取ajax发送的数组。
- */
-    @RequestMapping(value = "/array",method = RequestMethod.GET)
+
+    /*
+    两种方式获取ajax发送的数组。
+     */
+    @RequestMapping(value = "/array", method = RequestMethod.GET)
     @ResponseBody
-    public void Entity2(@RequestParam Map<String,String> map,HttpServletRequest request)
-    {
-        String arr[]=request.getParameterValues("arr[]");
-        for(String s:arr)
-        {
+    public void Entity2(@RequestParam Map<String, String> map, HttpServletRequest request) {
+        String arr[] = request.getParameterValues("arr[]");
+        for (String s : arr) {
             System.out.println(s);
         }
         /*
         只能获取到数组的第一个值
          */
-        System.out.println("所有name的map集合"+map);
-        System.out.println("map="+map.get("arr[]"));
+        System.out.println("所有name的map集合" + map);
+        System.out.println("map=" + map.get("arr[]"));
     }
+
     /*
     这种方式要注意数组要写[]
      */
-    @RequestMapping(value = "/array1",method = RequestMethod.GET,produces = "text/html;charset=UTF-8")
+    @RequestMapping(value = "/array1", method = RequestMethod.GET, produces = "text/html;charset=UTF-8")
     @ResponseBody
-    public String Entity1(@RequestParam("arr[]")int []arr)
-    {    for(int s:arr)
-    {
-        System.out.println(s);
-    }
+    public String Entity1(@RequestParam("arr[]") int[] arr) {
+        for (int s : arr) {
+            System.out.println(s);
+        }
         return "中文";
 
     }
+
     /*
     接受json数组对象(后台接受对象数组)
      */
-    @RequestMapping(value = "/testjson",method = RequestMethod.POST)
+    @RequestMapping(value = "/testjson", method = RequestMethod.POST)
     @ResponseBody
     public int saveUser(@RequestBody User[] user) {
-       System.out.println("-------------------");
-        for (User s:user
-             ) {
+        System.out.println("-------------------");
+        for (User s : user
+                ) {
             System.out.println(s);
         }
         /*
@@ -101,34 +99,35 @@ public class UserController {
         return 0;
     }
 
-    @RequestMapping(value = "/testjson1",method = RequestMethod.POST)
+    @RequestMapping(value = "/testjson1", method = RequestMethod.POST)
     @ResponseBody
     public Object saveUser() {
-        User u=new User("2去","312");
-        HotSale h=new HotSale("12","中文");
-        Map<String,Object> map=new HashMap<String, Object>();
-        map.put("u",u);
-        map.put("h",h);
+        User u = new User("2去", "312");
+        HotSale h = new HotSale("12", "中文");
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("u", u);
+        map.put("h", h);
         return map;
     }
-/*
-map接收json--
- */
-  @RequestMapping(value = "/test1",method = RequestMethod.POST)
-  @ResponseBody
-  public Object saveUser11(@RequestBody Map<String,Object> map ) {
-      try {
-          Object obj= map.get("user");
-          System.out.println("obj="+obj);
-        JSONObject jsonobject = JSONObject.fromObject(obj);
-        User user= (User)JSONObject.toBean(jsonobject,User.class);
-        userMapper.insert(user);
-          System.out.println(user);
-      }catch (Exception e){
-          System.out.println(e.getMessage());
-      }
 
-       return map;
-}
+    /*
+    map接收json--
+     */
+    @RequestMapping(value = "/test1", method = RequestMethod.POST)
+    @ResponseBody
+    public Object saveUser11(@RequestBody Map<String, Object> map) {
+        try {
+            Object obj = map.get("user");
+            System.out.println("obj=" + obj);
+            JSONObject jsonobject = JSONObject.fromObject(obj);
+            User user = (User) JSONObject.toBean(jsonobject, User.class);
+            userMapper.insert(user);
+            System.out.println(user);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        return map;
+    }
 
 }
