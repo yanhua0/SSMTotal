1e95020d7d3d9e892408d20e72f95a12d524b2ca
Administrator <1256656046@qq.com>
2019-02-18 16:14:08 +0800

    table.jsp乱码修复

--- src/main/java/org/total/web/TableController.java
+++ src/main/java/org/total/web/TableController.java
@@ -3,10 +3,13 @@ package org.total.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 public class TableController {
     @RequestMapping("/table")
-    public String table(){
+    public String table(HttpSession session){
+      session.setAttribute("test","1234");
         return "table";
     }
     @RequestMapping("/createExcel")
